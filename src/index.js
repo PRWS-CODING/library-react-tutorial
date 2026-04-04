55d73bc755c51e2d1cc7dc1aa0428c506303fbc6
dragontrucker33-code <dragontrucker33@gmail.com>
2026-04-04 14:51:30 -0400

    fixing node

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas, faShoppingCart, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+library.add(fas, faShoppingCart, faTimes);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
